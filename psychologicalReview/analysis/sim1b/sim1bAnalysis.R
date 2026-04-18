7c56c04093f95b5870edbf5fcaf6d3dfa0fb02bc
Deon T. Benton <deon.benton@vanderbilt.edu>
2026-04-18 00:02:51 -0500

    s

--- psychologicalReview/analysis/sim1b/sim1bAnalysis.R
+++ psychologicalReview/analysis/sim1b/sim1bAnalysis.R
@@ -28,7 +28,7 @@ library(Hmisc)
 
 
 
-setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/ALIAS/sim1b")
+setwd("C:/Users/detbe/Documents/projects/newGergelySim/psychologicalReview/data/ALIAS/sim1b")
 
 files = list.files(recursive = TRUE, full.names = TRUE)
 files = files[
@@ -63,11 +63,17 @@ D = do.call(rbind, data_list)
 D$ID = rep(1:1200, each = 2)
 
 # condition
-D$condition = rep(c("Control", "Experimental"), each = 120, times = 10)
 D$condition = as.factor(D$condition)
+D$condition = revalue(D$condition, c("control" = "Control",
+                                     "exp" = "Experimental"))
+
+D$condition = factor(D$condition, levels = c("Experimental",
+                                             "Control"))
 
 # trial type
-D$trialType = rep(c("Inefficient", "Efficient"), each = 1, times = 1200)
+D$trialType = as.factor(D$trial_type)
+D$trialType = revalue(D$trialType, c("consistent" = "Efficient",
+                                     "inconsistent" = "Inefficient"))
 
 # looking time 
 D$lookingTime = D$value
@@ -88,21 +94,19 @@ D$ratio = factor(D$ratio, levels = c("100% constrained vs. 0% unconstrained",
                                      "60% constrained vs. 40% unconstrained",
                                      "50% constrained vs. 50% unconstrained"))
 
-# order dataframe by ratio
-D = D[order(D$ratio),]
-
 # epochs
 D$epochs = as.factor(D$tEpochs)
 D$epochs = revalue(D$epochs, c("8" = "8 epochs", 
-                             "9" = "9 epochs",
-                             "10" = "10 epochs",
-                             "11" = "11 epochs",
-                             "12" = "12 epochs"))
+                               "9" = "9 epochs",
+                               "10" = "10 epochs",
+                               "11" = "11 epochs",
+                               "12" = "12 epochs"))
 
 D$epochs = factor(D$epochs, levels = c("8 epochs",
-                                     "9 epochs",
-                                     "10 epochs",
-                                     "11 epochs"))
+                                       "9 epochs",
+                                       "10 epochs",
+                                       "11 epochs",
+                                       "12 epochs"))
 
 
 # remove columns
@@ -116,20 +120,26 @@ is.data.frame(D)
 ####################
 
 # figure
-condition_barplot = ggplot(D, aes(condition, lookingTime, fill=trialType)) # create the bar graph with test.trial.2 on the x-axis and measure on the y-axis
-condition_barplot + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
+p = ggplot(D, aes(condition, lookingTime, fill=trialType)) + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
   stat_summary(fun.data=mean_cl_boot, geom = "errorbar", position = position_dodge(width=0.90), width = 0.2) + # add errors bars
   ylab("Network error") + # change the label of the y-axis
-  facet_wrap(~ratio, ncol=3) +
-  scale_y_continuous(expand = c(0, 0)) +
-  coord_cartesian(ylim=c(0, 200)) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+  coord_cartesian(ylim=c(0, 205)) +
+  facet_wrap(~ratio, ncol=3, labeller = label_wrap_gen(width = 25)) +
   scale_fill_manual(values = c("black", "azure3")) +
   labs(fill='Test Trial')  +
-  theme(axis.text.x = element_text(size = 25, angle = 20, hjust = 1),
-        axis.text.y = element_text(size = 25), 
-        legend.text=element_text(size=25),
-        legend.title = element_text(size=25),
-        axis.title=element_text(size=25),
+  theme(axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15), 
+        legend.text=element_text(size=15),
+        legend.title = element_text(size=15),
+        axis.title=element_text(size=15),
         strip.text = element_text(
-          size = 25), 
-        axis.title.x = element_blank())
+          size = 15), 
+        axis.title.x = element_blank(),
+        plot.margin = margin(3, 3, 3, 3)) +
+  theme(legend.position = "bottom")
+
+setwd("C:/Users/detbe/Documents/projects/newGergelySim/psychologicalReview/figures")
+ggsave("fig7_04172026_main.png", 
+       plot = p, 
+       width = 8.5, height = 5.2, dpi = 300)
