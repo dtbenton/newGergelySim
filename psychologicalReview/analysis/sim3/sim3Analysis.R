9ef37abaee9db19fe88230d300a71a1d68f19b21
Deon T. Benton <deon.benton@vanderbilt.edu>
2026-04-22 17:24:23 -0500

    s

--- psychologicalReview/analysis/sim3/sim3Analysis.R
+++ psychologicalReview/analysis/sim3/sim3Analysis.R
@@ -1,10 +1,10 @@
-###########################
-###########################
-##                       ##
-## SIMULATION 3 ANALYSIS ##
-##                       ##
-###########################
-###########################
+##################################
+##################################
+##                              ##
+## SIMULATION 3 ANALYSIS: ALIAS ##
+##                              ##
+##################################
+##################################
 
 
 # SIMULATION 3 ANALYSIS #
@@ -293,7 +293,7 @@ p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = 
         plot.margin = margin(3, 3, 3, 3),
         plot.caption = element_text(size = 18, hjust = 0.5)) +
   theme(legend.position = "none") +
-  labs(caption = "Inferring Goals condition") 
+  labs(caption = "Inferring Constraints condition") 
 
 setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
 ggsave("fig12_04212026_OM10_overall_constraints_main.png", 
@@ -364,5 +364,298 @@ p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = 
 
 setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
 ggsave("fig12_04212026_OM10_goals_main.png", 
+       plot = p, 
+       width = 6.5, height = 5.2, dpi = 300)
+
+
+
+#####################################
+#####################################
+##                                 ##
+## SIMULATION 3a ANALYSIS: OM10-2  ##
+##                                 ##
+#####################################
+#####################################
+
+###########################
+## Inferring Constraints ##
+###########################
+
+# set wd 
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/OverwalleModified/sim3a")
+
+# load data
+D = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+
+# get dimensionality of D
+dim(D)
+
+# create an ID column
+D$ID = rep(1:20, each = 2)
+
+
+# create trial type column
+D$trialType = rep(c("Obstacle", "No-Obstacle"), each = 1, times = 20)
+D$trialType = as.factor(D$trialType)
+
+D$trialType = factor(D$trialType, 
+                     levels = c("Obstacle", "No-Obstacle"))
+
+
+
+# create a 'looking time' column
+D$lookingTime = 1-D$V3
+
+# remove columns
+D = D[,c(4:6)]
+
+# confirm that it is a dataframe
+is.data.frame(D)
+
+####################
+## OMNIBUS FIGURE ##
+####################
+# main paper figure
+p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
+  stat_summary(fun.data=mean_cl_boot, geom = "errorbar", position = position_dodge(width=0.90), width = 0.2) + # add errors bars
+  ylab("Activation of Critical Units") + # change the label of the y-axis
+  scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+  coord_cartesian(ylim=c(0, 1.5)) +
+  scale_fill_manual(values = c("black", "azure3")) +
+  labs(fill='Test Trial')  +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12), 
+        legend.text=element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text = element_text(
+          size = 12), 
+        axis.title.x = element_blank(),
+        plot.margin = margin(3, 3, 3, 3),
+        plot.caption = element_text(size = 18, hjust = 0.5)) +
+  theme(legend.position = "none") +
+  labs(caption = "Inferring Constraints condition") 
+
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
+ggsave("fig12_04222026_OM10-2_overall_constraints_main.png", 
+       plot = p, 
+       width = 6.5, height = 5.2, dpi = 300)
+
+
+
+###########################
+#     Inferring Goals     #
+###########################
+
+
+# set wd 
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/OverwalleModified/sim3d")
+
+# load data
+D = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+
+# get dimensionality of D
+dim(D)
+
+# create an ID column
+D$ID = rep(1:20, each = 2)
+
+
+# create trial type column
+D$trialType = rep(c("Congruent", "Incongruent"), each = 1, times = 20)
+D$trialType = as.factor(D$trialType)
+
+D$trialType = factor(D$trialType, 
+                     levels = c("Congruent", "Incongruent"))
+
+
+
+# create a 'looking time' column
+D$lookingTime = 1-D$V3
+
+# remove columns
+D = D[,c(4:6)]
+
+# confirm that it is a dataframe
+is.data.frame(D)
+
+####################
+## OMNIBUS FIGURE ##
+####################
+# main paper figure
+p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
+  stat_summary(fun.data=mean_cl_boot, geom = "errorbar", position = position_dodge(width=0.90), width = 0.2) + # add errors bars
+  ylab("Activation of Critical Units") + # change the label of the y-axis
+  scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+  coord_cartesian(ylim=c(0, 0.95)) +
+  scale_fill_manual(values = c("black", "azure3")) +
+  labs(fill='Test Trial')  +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12), 
+        legend.text=element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text = element_text(
+          size = 12), 
+        axis.title.x = element_blank(),
+        plot.margin = margin(3, 3, 3, 3),
+        plot.caption = element_text(size = 18, hjust = 0.5)) +
+  theme(legend.position = "none") +
+  labs(caption = "Inferring Goals condition") 
+
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
+ggsave("fig12_04222026_OM10-2_goals_main.png", 
+       plot = p, 
+       width = 6.5, height = 5.2, dpi = 300)
+
+
+####################################
+####################################
+##                                ##
+## SIMULATION 3 ANALYSIS: ALIAS-2 ##
+##                                ##
+####################################
+####################################
+
+
+
+# CONSTRAINTS #
+
+# set working directory
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/ALIASnoSalience/sim3c")
+exp.8 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.9 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.10 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.11 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.12 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+
+
+# combine dataframes into a single 'D' data frame
+D = rbind(exp.8, exp.9, exp.10, exp.11, exp.12)
+
+# get dimensionality of D
+dim(D)
+
+# create an ID column
+D$ID = rep(1:100, each = 2)
+
+# create trial type column
+D$trialType = rep(c("Incongruent", "Congruent"), each = 1, times = 100)
+D$trialType = as.factor(D$trialType)
+
+D$trialType = factor(D$trialType, 
+                     levels = c("Congruent", 
+                                "Incongruent"))
+
+# epochs
+D$epochs = rep(c("8 Epochs", "9 Epochs", "10 Epochs", "11 Epochs", "12 Epochs"), 
+               each = 40)
+D$epochs = as.factor(D$epochs)
+
+D$epochs = factor(D$epochs, levels = c("8 Epochs", "9 Epochs", 
+                                       "10 Epochs", "11 Epochs", "12 Epochs"))
+
+# create a 'looking time' column
+D$lookingTime = D$V3
+
+# remove columns
+D = D[,-c(1:3)]
+
+
+# overall figure
+p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
+  stat_summary(fun.data=mean_cl_boot, geom = "errorbar", position = position_dodge(width=0.90), width = 0.2) + # add errors bars
+  ylab("Network error") + # change the label of the y-axis
+  scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+  coord_cartesian(ylim=c(0, 140)) +
+  scale_fill_manual(values = c("black", "azure3")) +
+  labs(fill='Test Trial')  +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12), 
+        legend.text=element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text = element_text(
+          size = 12), 
+        axis.title.x = element_blank(),
+        plot.margin = margin(3, 3, 3, 3),
+        plot.caption = element_text(size = 18, hjust = 0.5)) +
+  theme(legend.position = "none") +
+  labs(caption = "Inferring Goals condition")
+
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
+ggsave("fig13a_04222026_ALIAS-2__goals_main.png", 
+       plot = p, 
+       width = 6.5, height = 5.2, dpi = 300)
+
+
+###################
+### CONSTRAINTS ###
+###################
+# set working directory
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/ALIASnoSalience/sim3f")
+exp.8 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.9 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.10 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.11 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+exp.12 = read.table(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+
+# combine dataframes into a single 'D' data frame
+D = rbind(exp.8, exp.9, exp.10, exp.11, exp.12)
+
+# get dimensionality of D
+dim(D)
+
+# get dimensionality of D
+dim(D)
+
+# create an ID column
+D$ID = rep(1:100, each = 2)
+
+# create trial type column
+D$trialType = rep(c("No-Obstacle", "Obstacle"), each = 1, times = 100)
+D$trialType = as.factor(D$trialType)
+
+D$trialType = factor(D$trialType, 
+                     levels = c("Obstacle", 
+                                "No-Obstacle"))
+
+# epochs
+D$epochs = rep(c("8 Epochs", "9 Epochs", "10 Epochs", "11 Epochs", "12 Epochs"), 
+               each = 40)
+D$epochs = as.factor(D$epochs)
+
+D$epochs = factor(D$epochs, levels = c("8 Epochs", "9 Epochs", 
+                                       "10 Epochs", "11 Epochs", "12 Epochs"))
+
+# create a 'looking time' column
+D$lookingTime = D$V3
+
+# remove columns
+D = D[,-c(1:3)]
+
+# overall figure
+p = ggplot(D, aes(trialType, lookingTime, fill=trialType)) + stat_summary(fun = mean, geom = "bar", position = "dodge") + # add the bars, which represent the means and the place them side-by-side with 'dodge'
+  stat_summary(fun.data=mean_cl_boot, geom = "errorbar", position = position_dodge(width=0.90), width = 0.2) + # add errors bars
+  ylab("Network error") + # change the label of the y-axis
+  scale_y_continuous(expand = expansion(mult = c(0, 0.02))) +
+  coord_cartesian(ylim=c(0, 75)) +
+  scale_fill_manual(values = c("black", "azure3")) +
+  labs(fill='Test Trial')  +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12), 
+        legend.text=element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text = element_text(
+          size = 12), 
+        axis.title.x = element_blank(),
+        plot.margin = margin(3, 3, 3, 3),
+        plot.caption = element_text(size = 18, hjust = 0.5)) +
+  theme(legend.position = "none") +
+  labs(caption = "Inferring Constraints condition")
+
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/figures")
+ggsave("fig13a_04222026_ALIAS-2__constraints_main.png", 
        plot = p, 
        width = 6.5, height = 5.2, dpi = 300)
---
+++ psychologicalReview/overWalleModel/overwalleModelModified_csibraInferringGoal.R
@@ -0,0 +1,468 @@
+###############################################################################
+# MODIEF VAN OVERWALLE-STYLE SIMULATION OF THE INFERRING GOALS CONDITION 
+# (SANS OVERSHOOTING)
+# RECAST IN A GERGELY-STYLE OBSTACLE SETUP
+#
+# WHAT THIS SCRIPT DOES
+# ---------------------
+# Habituation:
+#   - The agent follows a jumping path over a center obstacle.
+#   - The obstacle is present.
+#   - NO goal information is provided during habituation.
+#
+# Test:
+#   - The agent follows a straight path in BOTH test events.
+#   - The obstacle is ABSENT at test, allowing a straight approach.
+#   - The only difference between test events is goal presence:
+#       * CONSISTENT   = goal present at the final step
+#       * INCONSISTENT = goal absent throughout the test event
+#
+# IMPORTANT:
+#   This is an adaptation of the Overwalle (2010) architecture to capture
+#   the logic of the Inferring Goals condition in Csibra et al. (2003),
+#   but within a Gergely-style obstacle framework.
+#
+#   In this version, the habituation jump path overshoots the obstacle,
+#   consistent with the original Overwalle-style implementation.
+###############################################################################
+
+
+###############################################################################
+# SECTION 1 — GLOBAL MODEL PARAMETERS
+###############################################################################
+
+GRID_ROWS = 5
+GRID_COLS = 5
+
+ETA = 0.10
+N_CYCLES = 4
+
+WMIN = -0.2
+WMAX =  0.2
+
+N_HABITUATION = 10
+N_PARTICIPANTS = 20
+
+# Capture and print the random seed used
+seed_used = sample.int(.Machine$integer.max, 1)
+set.seed(seed_used)
+print(seed_used)
+
+
+###############################################################################
+# SECTION 2 — UNIT NAMING
+###############################################################################
+
+unit_name = function(r, c) {
+  paste0("R", r, "C", c)
+}
+
+all_unit_names = function() {
+  
+  grid = character()
+  
+  for (r in 1:GRID_ROWS) {
+    for (c in 1:GRID_COLS) {
+      grid = c(grid, unit_name(r, c))
+    }
+  }
+  
+  c("Agent", "Goal", grid)
+}
+
+UNITS = all_unit_names()
+N_UNITS = length(UNITS)
+
+
+###############################################################################
+# SECTION 3 — GRID NEIGHBOR RELATIONS
+###############################################################################
+
+orth_neighbors = function(r, c) {
+  
+  candidates = rbind(
+    c(r + 1, c),
+    c(r - 1, c),
+    c(r, c + 1),
+    c(r, c - 1)
+  )
+  
+  keep =
+    candidates[, 1] >= 1 &
+    candidates[, 1] <= GRID_ROWS &
+    candidates[, 2] >= 1 &
+    candidates[, 2] <= GRID_COLS
+  
+  candidates[keep, , drop = FALSE]
+}
+
+
+###############################################################################
+# SECTION 4 — INITIALIZE WEIGHTS
+###############################################################################
+
+init_weights = function() {
+  
+  W = matrix(
+    runif(N_UNITS * N_UNITS, min = WMIN, max = WMAX),
+    nrow = N_UNITS,
+    ncol = N_UNITS,
+    dimnames = list(UNITS, UNITS)
+  )
+  
+  diag(W) = 0
+  
+  W
+}
+
+
+###############################################################################
+# SECTION 5 — EXTERNAL PATTERN ENCODING
+###############################################################################
+# Goal activation is controlled by goalOnThisStep.
+#
+# Rules:
+#   - Agent = +1 on every step
+#   - current location = +1
+#   - orthogonal neighbors = +0.5
+#   - obstacle neighbors = -1
+#   - Goal = +1 only when goalOnThisStep = TRUE
+###############################################################################
+
+make_external_pattern = function(cell,
+                                 obstacleCells = character(),
+                                 goalOnThisStep = FALSE) {
+  
+  ext = setNames(rep(0, N_UNITS), UNITS)
+  
+  ext["Agent"] = 1
+  
+  r = cell[1]
+  c = cell[2]
+  cur = unit_name(r, c)
+  
+  ext[cur] = 1
+  
+  nbrs = orth_neighbors(r, c)
+  
+  if (nrow(nbrs) > 0) {
+    for (i in 1:nrow(nbrs)) {
+      
+      rr = nbrs[i, 1]
+      cc = nbrs[i, 2]
+      nm = unit_name(rr, cc)
+      
+      if (nm %in% obstacleCells) {
+        ext[nm] = -1
+      } else {
+        ext[nm] = 0.5
+      }
+    }
+  }
+  
+  if (goalOnThisStep) {
+    ext["Goal"] = 1
+  }
+  
+  ext
+}
+
+
+###############################################################################
+# SECTION 6 — RECURRENT SETTLING
+###############################################################################
+
+settle_internal = function(ext, W, n_cycles = N_CYCLES) {
+  
+  int = setNames(rep(0, N_UNITS), UNITS)
+  
+  for (cycle in 1:n_cycles) {
+    sender = ext + int
+    int = as.vector(sender %*% W)
+    names(int) = UNITS
+  }
+  
+  int
+}
+
+
+###############################################################################
+# SECTION 7 — DELTA RULE LEARNING
+###############################################################################
+
+delta_update = function(W, ext, int) {
+  
+  sender = ext + int
+  error = ext - int
+  
+  dW = ETA * outer(sender, error)
+  dimnames(dW) = dimnames(W)
+  
+  W_new = W + dW
+  diag(W_new) = 0
+  
+  W_new
+}
+
+
+###############################################################################
+# SECTION 8 — RUN A SINGLE MOVEMENT STEP
+###############################################################################
+
+run_step = function(W, cell, obstacleCells, goalOnThisStep) {
+  
+  ext = make_external_pattern(
+    cell = cell,
+    obstacleCells = obstacleCells,
+    goalOnThisStep = goalOnThisStep
+  )
+  
+  int = settle_internal(ext, W)
+  W = delta_update(W, ext, int)
+  
+  list(W = W, ext = ext, int = int)
+}
+
+
+###############################################################################
+# SECTION 9 — TRAJECTORY DEFINITIONS
+###############################################################################
+# straight_path():
+#   used at test in both conditions
+#
+# jumping_path():
+#   used during habituation
+#   overshoots the obstacle in the original Overwalle-style manner
+###############################################################################
+
+straight_path = function() {
+  list(
+    c(1, 1),
+    c(1, 2),
+    c(1, 3),
+    c(1, 4),
+    c(1, 5)
+  )
+}
+
+jumping_path = function() {
+  list(
+    c(1, 1),
+    c(1, 2),
+    c(2, 2),
+    c(3, 2),
+    c(3, 3),
+    c(3, 4),
+    c(2, 4),
+    c(1, 4),
+    c(1, 5)
+  )
+}
+
+
+###############################################################################
+# SECTION 10 — ENVIRONMENT DEFINITIONS
+###############################################################################
+
+# Center obstacle used during habituation
+obstacle_cells_present = function() {
+  c(unit_name(1, 3), unit_name(1, 3))
+}
+
+obstacle_cells_absent = function() {
+  character()
+}
+
+
+###############################################################################
+# SECTION 11 — RUN A WHOLE TRIAL
+###############################################################################
+# goalStep:
+#   - NA           -> goal never appears
+#   - length(path) -> goal appears on final step
+###############################################################################
+
+run_trial = function(W, path, obstacleCells, goalStep = NA) {
+  
+  for (k in seq_along(path)) {
+    
+    goalOnThisStep = (!is.na(goalStep) && k == goalStep)
+    
+    out = run_step(
+      W = W,
+      cell = path[[k]],
+      obstacleCells = obstacleCells,
+      goalOnThisStep = goalOnThisStep
+    )
+    
+    W = out$W
+  }
+  
+  W
+}
+
+
+###############################################################################
+# SECTION 12 — PREDICTION / EXPECTATION PROBE
+###############################################################################
+# The probe turns on Agent and a weak Goal signal and reads activation
+# in critical cells.
+###############################################################################
+
+prediction_score = function(W, criticalCells) {
+  
+  ext = setNames(rep(0, N_UNITS), UNITS)
+  ext["Agent"] = 1
+  ext["Goal"] = 0.4
+  
+  int = settle_internal(ext, W)
+  
+  mean(int[criticalCells])
+}
+
+
+###############################################################################
+# SECTION 13 — CRITICAL CELLS
+###############################################################################
+# Because the test events use a straight path, score the straight-path cells.
+###############################################################################
+
+straight_critical_cells = function() {
+  c(unit_name(1, 2), unit_name(1, 3), unit_name(1, 4))
+}
+
+
+###############################################################################
+# SECTION 14 — RUN ONE EXPERIMENTAL CONDITION
+###############################################################################
+# testType:
+#   "consistent"   -> straight test path, no obstacle, goal present at end
+#   "inconsistent" -> straight test path, no obstacle, goal absent throughout
+#
+# Habituation:
+#   - jump path
+#   - center obstacle present
+#   - NO goal information
+###############################################################################
+
+run_condition = function(testType) {
+  
+  W = init_weights()
+  
+  # Habituation: jumping path, obstacle present, no goal information
+  habObs = obstacle_cells_present()
+  
+  for (t in 1:N_HABITUATION) {
+    W = run_trial(
+      W = W,
+      path = jumping_path(),
+      obstacleCells = habObs,
+      goalStep = NA
+    )
+  }
+  
+  # Test: straight path, no obstacle
+  testObs = obstacle_cells_absent()
+  
+  if (testType == "consistent") {
+    
+    # Full path: reaches goal
+    path = straight_path()
+    goalStep = length(path)
+    
+  } else if (testType == "inconsistent") {
+    
+    # Shorter path: stops before goal
+    full_path = straight_path()
+    path = full_path[1:(length(full_path) - 1)]
+    
+    # Goal appears AFTER agent stops (i.e., final step of shortened path)
+    goalStep = length(path)
+    
+  } else {
+    stop("testType must be 'consistent' or 'inconsistent'")
+  }
+  
+  W = run_trial(
+    W = W,
+    path = path,
+    obstacleCells = testObs,
+    goalStep = goalStep
+  )
+  
+  score = prediction_score(W, straight_critical_cells())
+  
+  score
+}
+
+
+###############################################################################
+# SECTION 15 — RUN THE FULL SIMULATION ACROSS PARTICIPANTS
+###############################################################################
+
+simulate_all = function() {
+  
+  results = data.frame(
+    participant = integer(),
+    condition = character(),
+    score = numeric(),
+    stringsAsFactors = FALSE
+  )
+  
+  for (p in 1:N_PARTICIPANTS) {
+    
+    results = rbind(
+      results,
+      data.frame(
+        participant = p,
+        condition = "consistent",
+        score = run_condition("consistent"),
+        stringsAsFactors = FALSE
+      )
+    )
+    
+    results = rbind(
+      results,
+      data.frame(
+        participant = p,
+        condition = "inconsistent",
+        score = run_condition("inconsistent"),
+        stringsAsFactors = FALSE
+      )
+    )
+  }
+  
+  results
+}
+
+
+###############################################################################
+# SECTION 16 — SUMMARIZE RESULTS
+###############################################################################
+
+summarize_results = function(results) {
+  aggregate(score ~ condition, data = results, FUN = mean)
+}
+
+
+###############################################################################
+# SECTION 17 — RUN THE MODEL
+###############################################################################
+
+results = simulate_all()
+cat("Condition means:\n")
+print(summarize_results(results))
+
+# To inspect participant-level results:
+# fix(results)
+
+# set working directory to move data below to the right folder
+setwd("C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/OverwalleModified/sim3d")
+# save data to text file
+write.table(results,
+            file = "C:/Users/bentod2/Documents/projects/current/NEWgergliuSims/psychologicalReview/data/OverwalleModified/sim3d/overwalleModifed_sim3d.txt",
+            sep = " ",
+            row.names = FALSE,
+            col.names = FALSE,
+            quote = FALSE
+)
